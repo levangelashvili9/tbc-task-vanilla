81110d562cd69a410f82bbf97c6a12af7717ceab
Levan Gelashvili <levan.gelashvili9@gmail.com>
2024-01-22 18:45:41 +0400

    feat: add courses config

--- src/config/index.ts
+++ src/config/index.ts
@@ -4,3 +4,60 @@ export const NavbarConfig = [
   { id: 2, title: "TBC x USAID" },
   { id: 3, title: "რისკები" },
 ];
+
+export const CoursesConfig = [
+  {
+    id: 0,
+    title: "iOS Development",
+    image: "/images/ios-development.png",
+    status: "რეგისტრაცია დასრულებულია",
+  },
+  {
+    id: 1,
+    title: "React",
+    image: "/images/react.png",
+    status: "რეგისტრაცია დასრულებულია",
+  },
+  {
+    id: 2,
+    title: "Intro to Python",
+    image: "/images/intro-to-python.png",
+    status: "რეგისტრაცია დასრულებულია",
+  },
+  {
+    id: 3,
+    title: "Advanced Python",
+    image: "/images/advanced-python.png",
+    status: "რეგისტრაცია დასრულებულია",
+  },
+  {
+    id: 4,
+    title: "Advanced Cybersecurity Course",
+    image: "/images/advanced-cyber-security.png",
+    status: "რეგისტრაცია დასრულებულია",
+  },
+  {
+    id: 5,
+    title: "ToT - Training of Trainers",
+    image: "/images/training-of-trainers.png",
+    status: "რეგისტრაცია დასრულებულია",
+  },
+  {
+    id: 6,
+    title: "Blockchain",
+    image: "/images/blockchain.png",
+    status: "რეგისტრაცია დასრულებულია",
+  },
+  {
+    id: 7,
+    title: "DevOps",
+    image: "/images/devops.png",
+    status: "რეგისტრაცია დასრულებულია",
+  },
+  {
+    id: 8,
+    title: "Information Security Governance",
+    image: "/images/information-security-governance.png",
+    status: "რეგისტრაცია დასრულებულია",
+  },
+];
