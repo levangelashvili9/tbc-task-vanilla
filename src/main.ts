af4737993cab5ee4370c78e66a754595da252578
Levan Gelashvili <levan.gelashvili9@gmail.com>
2024-01-22 18:49:27 +0400

    feat: add new sections

--- src/main.ts
+++ src/main.ts
@@ -4,5 +4,7 @@ import "./components";
 document.querySelector<HTMLDivElement>("#app")!.innerHTML = `
   <main>
     <navigation-bar></navigation-bar>
+    <home-hero></home-hero>
+    <tech-courses></tech-courses>
   </main>
 `;
