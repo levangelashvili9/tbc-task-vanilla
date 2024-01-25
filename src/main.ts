6f66f423d6d51c812fae56b401adde232cdfe038
Levan Gelashvili <levan.gelashvili9@gmail.com>
2024-01-25 18:42:25 +0400

    refactor: put navbar and footer out of main tag

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,13 @@
 import "./style.css";
 import "./components";
 
-document.querySelector<HTMLDivElement>("#app")!.innerHTML = `
+document.querySelector<HTMLDivElement>("#app")!.innerHTML = /* HTML */ `
+  <the-navbar></the-navbar>
   <main>
-    <the-navbar></the-navbar>
     <home-hero></home-hero>
     <home-courses></home-courses>
     <home-partners></home-partners>
     <home-faq></home-faq>
-    <the-footer></the-footer>
   </main>
+  <the-footer></the-footer>
 `;
